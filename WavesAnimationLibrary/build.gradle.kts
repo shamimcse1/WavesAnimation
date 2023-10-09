92415258a6a4b565407269e93f9d20bf781dd77c
Md Shamim Hossain <mdshamimh529@gmail.com>
2023-10-09 09:18:42 +0600

    All OK

--- WavesAnimationLibrary/build.gradle.kts
+++ WavesAnimationLibrary/build.gradle.kts
@@ -1,3 +1,4 @@
+import org.gradle.internal.impldep.com.amazonaws.util.IOUtils.release
 
 plugins {
     id("com.android.library")
@@ -27,11 +28,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -40,26 +41,16 @@ android {
         kotlinCompilerExtensionVersion = "1.4.8"
     }
     publishing {
-        singleVariant("release") {
-            withSourcesJar()
-            withJavadocJar()
+        publishing {
+            singleVariant("release") {
+                withSourcesJar()
+                withJavadocJar()
+            }
         }
     }
 
 }
-//afterEvaluate {
-//    publishing {
-//        publications {
-//            create<MavenPublication>("maven") {
-//                groupId = "com.github.shamimcse1"
-//                artifactId = "waves_animation_library"
-//                version = "1.0.0"
-//
-//                from(components["release"])
-//            }
-//        }
-//    }
-//}
+
 
 publishing {
     publications {
@@ -77,7 +68,7 @@ publishing {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     testImplementation("junit:junit:4.13.2")
--- app/src/main/java/com/example/wavesanimation/MainActivity.kt
+++ app/src/main/java/com/example/wavesanimation/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.codercamp.wavesanimationlibrary.WavesAnimation
 import com.example.wavesanimation.ui.theme.WavesAnimationTheme
 
 class MainActivity : ComponentActivity() {
@@ -44,7 +45,7 @@ fun GreetingPreview() {
         Surface(
             color = MaterialTheme.colorScheme.tertiary
         ) {
-            //WavesAnimation()
+            WavesAnimation()
         }
 
     }
